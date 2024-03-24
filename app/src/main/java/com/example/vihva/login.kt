63258302c2094f2e8a0a678e69a29d627c5b0094
Pedrolbtb <pedro.lbtb@gmail.com>
2024-03-23 21:26:07 -0300

    botão de paciente funcionando e troca do text por hint

--- app/src/main/java/com/example/vihva/login.kt
+++ app/src/main/java/com/example/vihva/login.kt
@@ -1,11 +1,26 @@
 package com.example.vihva
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        findViewById<Button>(R.id.btn_pac).setOnClickListener {
+
+            irParaTelaLoginP()
+
+        }
+
+    }
+
+    private fun irParaTelaLoginP() {
+
+        val telaL = Intent(this, MainActivity::class.java)
+        startActivity(telaL)
     }
 }
