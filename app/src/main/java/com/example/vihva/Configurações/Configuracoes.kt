b15d0ac8509c99fa4921a1de72ba0891840e08a5
Pedrolbtb <pedro.lbtb@gmail.com>
2024-04-16 14:36:55 -0300

    sos

---
+++ app/src/main/java/com/example/vihva/Configurações/Configuracoes.kt
@@ -0,0 +1,20 @@
+package com.example.vihva.Configurações
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.vihva.R
+
+class Configuracoes : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_configuracoes)
+
+        val intent = intent
+        val name = intent.getStringExtra("name")
+        val phone = intent.getIntExtra("phone", 0)
+
+        Toast.makeText(this, name, Toast.LENGTH_SHORT).show()
+
+    }
+}
--- app/src/main/java/com/example/vihva/Inicio/Inicio.kt
+++ app/src/main/java/com/example/vihva/Inicio/Inicio.kt
@@ -1,12 +1,24 @@
 package com.example.vihva.Inicio
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.example.vihva.Configurações.Configuracoes
+import com.example.vihva.CriarPerfil.CriaPerfil2
 import com.example.vihva.R
 
 class Inicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_inicio)
+    }//fim do oncreate
+
+    fun irParaTelaConfig(View:View) {
+        val telaL = Intent(this, Configuracoes::class.java)
+        telaL.putExtra("name", "Batman")
+        telaL.putExtra("phone", 98955)
+        startActivity(telaL)
     }
-}
+
+}//fim da classe
