a0284eea2451f28c5a7ac31f4a676d0c51400b9d
samuel bueno <samuelbueno467@gmail.com>
2024-04-17 08:24:50 -0300

    intents de dados do cadastro criadas

--- app/src/main/java/com/example/vihva/CriarPerfil/CriaPerfil2.kt
+++ app/src/main/java/com/example/vihva/CriarPerfil/CriaPerfil2.kt
@@ -9,6 +9,8 @@ import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.NumberPicker
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -17,6 +19,7 @@ import com.example.vihva.R
 
 class CriaPerfil2 : AppCompatActivity() {
 
+    // Declarando as variáveis para os elementos da UI
     private lateinit var edit_peso: EditText
     private lateinit var np_peso: NumberPicker
     private lateinit var numberPickerView: View
@@ -27,12 +30,15 @@ class CriaPerfil2 : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cria_perfil2)
 
+        // Inicializando as variáveis com referências aos elementos da UI
         val edit_altura: EditText = findViewById(R.id.edit_altura)
         edit_peso = findViewById(R.id.edit_peso)
         np_peso = findViewById(R.id.np_peso)
 
+        // Inflando a view do NumberPicker
         numberPickerView = layoutInflater.inflate(R.layout.number_picker_layout, null)
 
+        // Criando o AlertDialog para o NumberPicker
         alertDialog = AlertDialog.Builder(this)
             .setView(numberPickerView)
             .setTitle("Selecione o peso em kg")
@@ -44,6 +50,7 @@ class CriaPerfil2 : AppCompatActivity() {
             .setNegativeButton("Cancelar", null)
             .create()
 
+        // Configurando o clique no EditText de peso para exibir o NumberPicker
         edit_peso.setOnClickListener {
             val np_peso = numberPickerView.findViewById<NumberPicker>(R.id.np_peso)
             np_peso.minValue = 30
@@ -52,13 +59,16 @@ class CriaPerfil2 : AppCompatActivity() {
             alertDialog.show()
         }
 
+        // Desabilitando o teclado virtual ao clicar no EditText de peso
         edit_peso.showSoftInputOnFocus = false
 
+        // Configurando o listener para o NumberPicker
         np_peso.setOnValueChangedListener { picker, oldVal, newVal ->
             UltimoValor = newVal
             edit_peso.setText(newVal.toString())
         }
 
+        // Configurando o TextWatcher para o EditText de altura
         edit_altura.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {}
 
@@ -75,25 +85,42 @@ class CriaPerfil2 : AppCompatActivity() {
             }
         })
 
+        // Configurando o clique no botão de "Próximo"
         findViewById<Button>(R.id.btn_proximo).setOnClickListener {
+            // Obtendo os valores de altura, peso e gênero
             val altura = edit_altura.text.toString().removeSuffix("cm").toIntOrNull()
             val peso = edit_peso.text.toString().removeSuffix("kg").toIntOrNull()
+            val genero = when (findViewById<RadioGroup>(R.id.radioGroup).checkedRadioButtonId){
+                R.id.radio_fem -> "Feminino"
+                R.id.radio_masc -> "Masculino"
+                R.id.radio_gen -> "Sem genero"
+                else -> null
+            }
 
+            // Validando os dados inseridos pelo usuário
             if (altura != null && altura >= 100 && altura <= 300 && peso != null){
-                irParaTelaInicial()
+                // Criando a Intent e iniciando a próxima atividade
+                val intent = Intent(this, FotoBio::class.java)
+                intent.putExtra("altura",altura)
+                intent.putExtra("peso",peso)
+                intent.putExtra("genero",genero)
+                startActivity(intent)
+                finish() // Finalizando a atividade atual
             }else{
-                // Utilizando o Toast personalizado
+                // Exibindo um Toast caso os dados não sejam válidos
                 showToast("Digite um valor de altura entre 100cm e 300cm ou insira um peso")
             }
         }
     }
 
+    // Função para navegar para a tela inicial
     private fun irParaTelaInicial() {
         val telaL = Intent(this, FotoBio::class.java)
         startActivity(telaL)
         finish()
     }
 
+    // Liberando recursos quando a atividade é destruída
     override fun onDestroy() {
         super.onDestroy()
         alertDialog.dismiss()
