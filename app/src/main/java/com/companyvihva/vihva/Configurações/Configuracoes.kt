6bacd2f57a1f04232d8d353a1922fc0c43469610
BuenoSamu <160734766+BuenoSamu@users.noreply.github.com>
2024-05-12 11:52:57 -0300

    tela de deletar conta

--- app/src/main/java/com/companyvihva/vihva/Configurações/Configuracoes.kt
+++ app/src/main/java/com/companyvihva/vihva/Configurações/Configuracoes.kt
@@ -1,6 +1,7 @@
 package com.companyvihva.vihva.Configurações
 
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -10,65 +11,61 @@ import android.widget.Spinner
 import android.widget.TextView
 import android.widget.Toast
 import com.companyvihva.vihva.R
-import com.companyvihva.vihva.databinding.ActivityConfiguracoesBinding
+import com.companyvihva.vihva.Configurações.ActivityDeletarPerfil
+
+
+
+
 
 class Configuracoes : AppCompatActivity() {
 
     private lateinit var spinnerDDI: Spinner
-    private lateinit var  editTextPhone: EditText
-    private lateinit var  editTextMsg: EditText
+    private lateinit var editTextPhone: EditText
+    private lateinit var editTextMsg: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracoes)
 
-        // Inflar o layout usando o View Binding
-        //binding = ActivityConfiguracoesBinding.inflate(layoutInflater)
-        //setContentView(binding.root)
+        findViewById<Button>(R.id.btn_delete).setOnClickListener {
+           // // Redirecionar para a ActivityDeletarPerfil
+            startActivity(Intent(this, ActivityDeletarPerfil::class.java))
+        //    startActivity(ActivityDeletarPerfil)
+       // }
 
-
-        //obter um conjunto de preferências do app
+        // Obter um conjunto de preferências do app
         val preference = getSharedPreferences("vihva", Context.MODE_PRIVATE)
-
-        loadPreference(preference)
+       // loadPreference(preference)
 
         spinnerDDI = findViewById(R.id.spinerDDI)
         editTextPhone = findViewById(R.id.editTextPhone)
         editTextMsg = findViewById(R.id.editTextMsg)
 
-
-        //Salva as preferencias
+        // Salvar as preferências
         findViewById<Button>(R.id.btn_confirmar).setOnClickListener {
             preference.edit()
-            .putInt("ddi", spinnerDDI.selectedItemPosition)
-            .putLong("phone", editTextPhone.text.toString().toLong())
-            .putString("text_msg_padrao", editTextMsg.text.toString())
-            .apply()
+                .putInt("ddi", spinnerDDI.selectedItemPosition)
+                .putLong("phone", editTextPhone.text.toString().toLong())
+                .putString("text_msg_padrao", editTextMsg.text.toString())
+                .apply()
 
-            //exibe o toast de confirmação
+            // Exibir o toast de confirmação
             Toast.makeText(this, getString(R.string.Toast_configuracoes_sucesso), Toast.LENGTH_SHORT).show()
-
-
-        }// fim do save
-
-       findViewById<Button>(R.id.btn_restaurar).setOnClickListener {
-
-       }
-
-
         }
 
-         fun loadPreference(preferences: SharedPreferences){
-          val ddi = preferences.getInt("ddi",0)
-          val phone = preferences.getLong("phone",0)
-          val mensagem = preferences.getString("text_msg_padrao","")
-
-             //Exibe as preferencias
-             //spinnerDDI.setSelection(ddi)
-             //editTextPhone.setText(phone.toString())
-            // editTextMsg.setText(mensagem)
-
-
-    }//fim do onCreate
-
+        findViewById<Button>(R.id.btn_restaurar).setOnClickListener {
+            // Implementar a lógica de restaurar as configurações padrão aqui
+        }
     }
+
+   // private fun loadPreference(preferences: SharedPreferences) {
+        //val ddi = preferences.getInt("ddi", 0)
+       // val phone = preferences.getLong("phone", 0)
+       // val mensagem = preferences.getString("text_msg_padrao", "")
+
+        // Exibir as preferências
+        // spinnerDDI.setSelection(ddi)
+        // editTextPhone.setText(phone.toString())
+        // editTextMsg.setText(mensagem)
+    }
+}
