f6f79a76b5d9fdd11f50ad15f4a6408809aa901f
BuenoSamu <160734766+BuenoSamu@users.noreply.github.com>
2024-05-13 19:35:28 -0300

    e

--- app/src/main/java/com/companyvihva/vihva/CriarPerfil/FotoBio.kt
+++ app/src/main/java/com/companyvihva/vihva/CriarPerfil/FotoBio.kt
@@ -1,28 +1,32 @@
 package com.companyvihva.vihva.CriarPerfil
-
 import android.content.Intent
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import com.companyvihva.vihva.R
+import android.util.Log
 import android.widget.Toast
 import com.companyvihva.vihva.Inicio.Inicio
-import com.companyvihva.vihva.R
 import com.companyvihva.vihva.databinding.ActivityFotoBioBinding
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 import java.io.ByteArrayOutputStream
 
 class FotoBio : AppCompatActivity() {
+    // Declaração da propriedade lateinit para a imageView
     private lateinit var imageView: ImageView
     private lateinit var editTextBiografia: EditText
     private lateinit var contadorCaracteres: TextView
+    private var _binding: ActivityFotoBioBinding? = null
+    private val binding get() = _binding
     private val db = FirebaseFirestore.getInstance()
 
+    // Companion object para declarar uma constante para o código de solicitação de imagem
     companion object {
         val IMAGE_REQUEST_CODE = 100
         private const val TAG = "KotlinActivity"
@@ -30,11 +34,12 @@ class FotoBio : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_foto_bio)
-
+        setContentView(R.layout.activity_foto_bio) // Define o layout da atividade
+// Inicializa a imageView com base em seu ID no layout
         imageView = findViewById(R.id.img_save)
         editTextBiografia = findViewById(R.id.Edit_biografia)
 
+//Recuperando os extras da intent
         val nome = intent.getStringExtra("nome")
         val sobrenome = intent.getStringExtra("sobrenome")
         val idade = intent.getIntExtra("idade", 0)
@@ -42,6 +47,7 @@ class FotoBio : AppCompatActivity() {
         val peso = intent.getIntExtra("peso", 0)
         val genero = intent.getStringExtra("genero")
 
+//Exibindo os dados nas TextViews
         val textNome = findViewById<TextView>(R.id.text_nome)
         textNome.text = "$nome $sobrenome"
         val textIdade = findViewById<TextView>(R.id.text_idade)
@@ -53,10 +59,11 @@ class FotoBio : AppCompatActivity() {
         val textGenero = findViewById<TextView>(R.id.text_genero)
         textGenero.text = "$genero"
 
+// Define um ouvinte de clique para a imageView
         imageView.setOnClickListener {
-            pickImageGallery()
+            pickImageGallery() // Chama o método para selecionar uma imagem da galeria
         }
-
+// Configura o OnClickListener para o botão retornar
         findViewById<Button>(R.id.btn_retornar).setOnClickListener {
             val altura = intent.getIntExtra("altura", 0)
             val peso = intent.getIntExtra("peso", 0)
@@ -74,7 +81,6 @@ class FotoBio : AppCompatActivity() {
             startActivity(criaPerfil2)
             finish()
         }
-
         findViewById<Button>(R.id.btn_proximo).setOnClickListener {
             val intent = Intent(this, Inicio::class.java)
             val biografia = editTextBiografia.text.toString()
@@ -83,20 +89,32 @@ class FotoBio : AppCompatActivity() {
         }
     }
 
+    // Método para abrir a galeria de mídia e selecionar uma imagem
     private fun pickImageGallery() {
-        val intent = Intent(Intent.ACTION_PICK)
-        intent.type = "image/*"
-        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+        val intent = Intent(Intent.ACTION_PICK) // Criação de uma Intent para selecionar um item
+        intent.type =
+            "image/*" // Define o tipo de item a ser selecionado (neste caso, qualquer imagem)
+        startActivityForResult(
+            intent,
+            IMAGE_REQUEST_CODE
+        ) // Inicia uma atividade para selecionar um item e espera o resultado
     }
 
+    // Função chamada quando uma atividade iniciada por este aplicativo retorna um resultado
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+// Verifica se o resultado veio da seleção da imagem e se foi bem-sucedido
         if (requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+// Define a imagem selecionada no ImageView
             imageView.setImageURI(data.data)
+            imageView.clipToOutline = true
+// Define a propriedade clipToOutline do ImageView como true para aplicar a forma do contorno do ImageView
             imageView.clipToOutline = true
         }
     }
 
+    // Método para salvar os dados no banco de dados Firestore
+    // Método para salvar os dados no banco de dados Firestore
     private fun saveData(
         nome: String?,
         sobrenome: String?,
@@ -106,46 +124,35 @@ class FotoBio : AppCompatActivity() {
         peso: Int,
         biografia: String?
     ) {
-        val dadosCliente = HashMap<String, Any>()
-        dadosCliente["nome"] = nome!!
-        dadosCliente["sobrenome"] = sobrenome!!
-        dadosCliente["genero"] = genero!!
-        dadosCliente["idade"] = idade
-        dadosCliente["altura"] = altura
-        dadosCliente["peso"] = peso
-        dadosCliente["biografia"] = biografia!!
+        // Obter o UID do usuário autenticado
+        val uid = FirebaseAuth.getInstance().currentUser?.uid
 
-        //try {
-            // Convertendo a imagem em bytes
-           // val drawable = imageView.drawable
-           // val bitmap = (drawable as BitmapDrawable).bitmap
-           // val outputStream = ByteArrayOutputStream()
-           /// bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream)
-           // val imageData = outputStream.toByteArray()
-           // dadosCliente["bytesImagem"] = imageData
+        if (uid != null) {
+            // Cria um mapa com os dados adicionais do perfil
+            val dadosPerfil = hashMapOf(
+                "nome" to nome!!,
+                "sobrenome" to sobrenome!!,
+                "genero" to genero!!,
+                "idade" to idade,
+                "altura" to altura,
+                "peso" to peso,
+                "biografia" to biografia!!
+            )
 
-            // Adiciona os dados à coleção "clientes" no Firestore
-            db.collection("clientes").add(dadosCliente)
-                .addOnSuccessListener { documentReference ->
-                    Log.d(TAG, "Dados salvos com sucesso: $documentReference")
+            // Atualiza o documento existente com os dados adicionais do perfil
+            db.collection("clientes").document(uid).update(dadosPerfil)
+                .addOnSuccessListener {
                     Toast.makeText(this, "Dados salvos com sucesso", Toast.LENGTH_SHORT).show()
                 }
                 .addOnFailureListener { e ->
-                    Log.e(TAG, "Erro ao salvar os dados: ", e)
                     Toast.makeText(
                         this,
                         "Erro ao salvar os dados: ${e.message}",
                         Toast.LENGTH_SHORT
                     ).show()
                 }
-       // } catch (e: Exception) {
-         //   Log.e(TAG, "Erro ao converter a imagem em bytes: ", e)
-          //  Toast.makeText(
-           //     this,
-            //    "Erro ao converter a imagem em bytes: ${e.message}",
-             //   Toast.LENGTH_SHORT
-           // ).show()
+        } else {
+            Toast.makeText(this, "Usuário não autenticado", Toast.LENGTH_SHORT).show()
         }
     }
-//}
-
+}
