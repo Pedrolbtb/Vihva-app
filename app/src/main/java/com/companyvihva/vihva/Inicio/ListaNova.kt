67c0af8cb841e768a7c822346cfbde9cd4e273f6
BuenoSamu <160734766+buenosamu@users.noreply.github.com>
2024-05-29 22:53:06 -0300

    lista de remedios clicavel

--- app/src/main/java/com/companyvihva/vihva/Inicio/ListaNova.kt
+++ app/src/main/java/com/companyvihva/vihva/Inicio/ListaNova.kt
@@ -8,6 +8,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.companyvihva.vihva.model.Listanew
 import com.google.firebase.firestore.FirebaseFirestore
 import android.util.Log
+import kotlin.collections.emptyList
+import kotlin.collections.List
 
 class ListaNova : AppCompatActivity() {
 
@@ -38,14 +40,14 @@ class ListaNova : AppCompatActivity() {
 
     private fun fetchSubList(remedioId: String) {
         val subDocumentos = when (remedioId) {
-            "Insulina" -> listOf("humalog", "novolog", "lantus", "levemir")
-            "Metformina" -> listOf("glifage", "glucophage")
-            "sulfonilureias" -> listOf("glibenclamida", "glipizida", "glimepirida")
-            "inibidoresdpp4" -> listOf("sitagliptina","saxagliptina", "glinagliptina")
-            "inibidoresdeSGLT2" -> listOf("daplaglifozina", "enpagueliflozina", "canaglifozina")
-            "agonistasdoGLP1" -> listOf("exenatida", "Liraglutida", "dulaglutida")
-            "tiazolidinedionas" -> listOf("pioglitazona", "rosiglitazona")
-            "Inibidoresdaalfaglicosidase" -> listOf("acarbose", "Miglitol")
+            "Insulina" -> listOf("humalog", "novolog", "lantus", "levemir1")//ok
+            "Metformina" -> listOf("glifage", "glucophage")//ok
+            "sulfonilureias" -> listOf("glibenclamida", "glipizida", "glimepirida")//ok
+            "Inibidores Dpp4" -> listOf("sitagliptina","saxagliptina1", "glinagliptina")//ok
+            "Inibidores de SGLT2" -> listOf("dapagliflozina1", "empagliflozina ", "canagliflozina ")//ok
+            "Agonistas do GLP1" -> listOf("exenatida","liraglutida1", "dulaglutida1" )//ok
+            "Tiazolidinedionas" -> listOf("pioglitazona1","rosiglitazona1")//ok
+            "Inibidores de alfa glicosidase" -> listOf("acarbose1", "miglitol1")//ok
 
             // Adicione mais listas conforme necessário
             else -> emptyList()
