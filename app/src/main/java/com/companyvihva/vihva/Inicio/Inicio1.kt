faf81f2c4b6c2da051c391dbf49a9601b2fbae1b
BuenoSamu <160734766+BuenoSamu@users.noreply.github.com>
2024-06-15 16:03:50 -0300

    delete da lisat de remedio na tela inicial

--- app/src/main/java/com/companyvihva/vihva/Inicio/Inicio1.kt
+++ app/src/main/java/com/companyvihva/vihva/Inicio/Inicio1.kt
@@ -1,4 +1,5 @@
 package com.companyvihva.vihva.Inicio
+
 import android.Manifest
 import android.app.AlertDialog
 import android.content.pm.PackageManager
@@ -22,6 +23,7 @@ import androidx.appcompat.widget.AppCompatImageButton
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.findViewTreeViewModelStoreOwner
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.companyvihva.vihva.Configurações.Configuracoes
@@ -33,10 +35,10 @@ import com.companyvihva.vihva.model.Tipo_Remedios
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 import kotlinx.coroutines.NonDisposableHandle.parent
-
 
 class Inicio1 : Fragment() {
 
@@ -66,9 +68,7 @@ class Inicio1 : Fragment() {
         textAviso?.text = spannableString
 
         db = FirebaseFirestore.getInstance()
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
-
         btnSOS = view.findViewById(R.id.sos)
         btnSOS.setOnClickListener {
             requestPermissions(
@@ -80,124 +80,88 @@ class Inicio1 : Fragment() {
 
         setupRecyclerView(view)
         fetchRemediosDoUsuario()
-
         setupDiabetesInfo(view)
+
+        // Adicionando o listener para o botão de imagem de lixeira global
+        val imageLixeiraGlobal: ImageView = view.findViewById(R.id.image_lixeira_global)
+        imageLixeiraGlobal.setOnClickListener {
+            deletarArrayRemedios()
+        }
 
         return view
     }
-    ////////////////////////////////////////////////////////////////////////////////////////
-// Método setupRecyclerView
+
+    // Método setupRecyclerView
     private fun setupRecyclerView(view: View) {
-        // Encontra a RecyclerView no layout com o ID Recyclerview_remedioAdicionado
         val recyclerview_remedioAdicionado =
             view.findViewById<RecyclerView>(R.id.Recyclerview_remedioAdicionado)
-
-        // Define o layout manager para a RecyclerView como LinearLayoutManager vertical
         recyclerview_remedioAdicionado.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
-
-        // Define que a RecyclerView terá um tamanho fixo para otimizar o desempenho
         recyclerview_remedioAdicionado.setHasFixedSize(true)
-
-        // Inicializa uma lista vazia para conter os itens da RecyclerView
         listaInicio = mutableListOf()
-
-        // Inicializa o adapter da RecyclerView com a lista vazia
         adapterListanova = AdapterListanova(requireContext(), listaInicio)
-
-        // Define o adapter criado para a RecyclerView
         recyclerview_remedioAdicionado.adapter = adapterListanova
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////
-// Método fetchRemediosDoUsuario
-    // Método fetchRemediosDoUsuario para buscar os IDs dos remédios do usuário
+    // Método fetchRemediosDoUsuario
     private fun fetchRemediosDoUsuario() {
-        // Obtém o usuário atualmente logado
         val user = FirebaseAuth.getInstance().currentUser
         val uid = user?.uid
 
-        // Verifica se o UID do usuário não é nulo
         if (uid != null) {
-            // Obtém a referência do documento do cliente no Firestore
             val clientRef = db.collection("clientes").document(uid)
-
-            // Realiza a leitura assíncrona do documento do cliente
             clientRef.get()
                 .addOnSuccessListener { document ->
-                    // Verifica se o documento existe e contém dados
                     if (document != null && document.exists()) {
-                        // Obtém os IDs dos remédios do documento
                         val remediosIds = document.get("remedios") as? List<String>
-
-                        // Verifica se existem remédios para o usuário
                         remediosIds?.let {
-                            // Itera sobre os IDs dos remédios e chama o método para buscar os dados de cada um
                             for (remedioId in it) {
                                 fetchDadosDoFirebase(remedioId)
                             }
                         }
                     } else {
-                        // Loga uma mensagem de erro caso o documento do cliente não seja encontrado
                         Log.d("Inicio1", "Documento do cliente não encontrado")
                     }
                 }
                 .addOnFailureListener { e ->
-                    // Loga uma mensagem de erro em caso de falha ao obter o documento do cliente
                     Log.w("Inicio1", "Erro ao obter documento do cliente", e)
                 }
         }
     }
 
-    // Método fetchDadosDoFirebase para buscar os detalhes de cada remédio usando o ID
+    // Método fetchDadosDoFirebase
     private fun fetchDadosDoFirebase(remedioId: String) {
-        // Obtém a referência do documento do remédio no Firestore
         val docRef = db.collection("remedios").document(remedioId)
-
-        // Realiza a leitura assíncrona do documento do remédio
         docRef.get()
             .addOnSuccessListener { document ->
-                // Verifica se o documento existe e contém dados
                 if (document != null && document.exists()) {
-                    // Obtém os campos do documento
                     val nome = document.getString("nome")
                     val tipo = document.getString("tipo")
                     val descricao = document.getString("descricao")
                     val url = document.getString("Url")
 
-                    // Verifica se nome, tipo e descricao não são nulos antes de criar o objeto Tipo_Remedios
                     if (nome != null && tipo != null && descricao != null) {
-                        // Cria um objeto Tipo_Remedios com os dados obtidos
                         val remedio = Tipo_Remedios(url ?: "", nome, tipo, descricao)
-
-                        // Atualiza a lista de remédios e notifica o adapter sobre a mudança nos dados
                         atualizarListaRemedios(remedio)
                     } else {
-                        // Loga uma mensagem de aviso caso nome, tipo ou descricao do remédio sejam nulos
                         Log.d("Inicio1", "Nome, tipo ou descricao do remédio está nulo")
                     }
                 } else {
-                    // Loga uma mensagem de erro caso o documento do remédio não seja encontrado
                     Log.d("Inicio1", "Documento do remédio não encontrado")
                 }
             }
             .addOnFailureListener { e ->
-                // Loga uma mensagem de erro em caso de falha ao obter o documento do remédio
                 Log.w("Inicio1", "Erro ao obter documento do remédio", e)
             }
     }
 
-    // Método atualizarListaRemedios para adicionar o remédio à lista e notificar o adapter
+    // Método atualizarListaRemedios
     private fun atualizarListaRemedios(remedio: Tipo_Remedios) {
-        // Adiciona o remédio à lista de remédios
         listaInicio.add(remedio)
-
-        // Notifica o adapter que os dados mudaram para atualizar a RecyclerView
         adapterListanova.notifyDataSetChanged()
     }
 
-    ////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    // Método setupDiabetesInfo
     private fun setupDiabetesInfo(view: View) {
         val doencaRef = db.collection("doenca").document("diabetes")
         doencaRef.get()
@@ -231,6 +195,26 @@ class Inicio1 : Fragment() {
             }
     }
 
+    // Método para deletar o array de remédios do usuário autenticado
+    private fun deletarArrayRemedios() {
+        val user = FirebaseAuth.getInstance().currentUser
+        val uid = user?.uid
+
+        if (uid != null) {
+            val clientRef = db.collection("clientes").document(uid)
+            clientRef.update("remedios", FieldValue.delete())
+                .addOnSuccessListener {
+                    Toast.makeText(requireContext(), "Lista de remédios deletado com sucesso", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener { e ->
+                    Toast.makeText(requireContext(), "Erro ao deletar o lista de remédios: ${e.message}", Toast.LENGTH_SHORT).show()
+                }
+        } else {
+            Toast.makeText(requireContext(), "Erro: UID do usuário não encontrado", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Método requestPermissions
     private fun requestPermissions(vararg permissions: String) {
         if (ActivityCompat.checkSelfPermission(
                 requireContext(),
@@ -288,6 +272,6 @@ class Inicio1 : Fragment() {
                     Toast.LENGTH_SHORT
                 ).show()
             }
-            }
         }
+    }
 }
