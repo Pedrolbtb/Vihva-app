424a22abe1a50bc0bcdb484064ae6ba8f4574abc
BuenoSamu <160734766+BuenoSamu@users.noreply.github.com>
2024-07-05 16:08:13 -0300

    Exclusão individual dos itens da tela inicial

--- app/src/main/java/com/companyvihva/vihva/model/DescriçãoDoença_inicio1.kt
+++ app/src/main/java/com/companyvihva/vihva/model/DescriçãoDoença_inicio1.kt
@@ -2,24 +2,23 @@ package com.companyvihva.vihva.com.companyvihva.vihva.model
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.companyvihva.vihva.R
 import com.companyvihva.vihva.model.Tipo_Classe
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirestoreRegistrar
 import com.squareup.picasso.Picasso
 
-class DescriçãoDoença_inicio1: AppCompatActivity() {
-
+class DescriçãoDoença_inicio1 : AppCompatActivity() {
 
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     private lateinit var nomeTextView: TextView
     private lateinit var descricaoTextView: TextView
     private lateinit var urlImageView: ImageView
@@ -30,10 +29,11 @@ class DescriçãoDoença_inicio1: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.popup_desc_doenca_inicio1)
 
-        // Inicializa o Firestore
+        // Inicializa o Firestore e o auth
         firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-        // Obtem o UID do usuario atualmente cadastrado
+        // Obtém o ID da doença passada pelo Intent
         doencaid = intent.getStringExtra("doencaId")
 
         // Configura o botão de voltar
@@ -42,54 +42,82 @@ class DescriçãoDoença_inicio1: AppCompatActivity() {
             onBackPressed()
         }
 
-        //Referências para as TextViews que irão receber as informações da doença
+        // Configura o botão de excluir
+        val btnExcluir = findViewById<ImageButton>(R.id.lixeira_doencas)
+        btnExcluir.setOnClickListener {
+            doencaid?.let { id ->
+               showConfirmDeleteDialog(id)
+            }
+        }
+
+        // Referências para as TextViews que irão receber as informações da doença
         nomeTextView = findViewById(R.id.nomeDoenca)
         descricaoTextView = findViewById(R.id.descricao2)
         urlImageView = findViewById(R.id.foto_Doenca)
 
-        //Busca os dados do Firebase usando o Id da doença
+        // Busca os dados do Firebase usando o Id da doença
         doencaid?.let { id ->
             fetchDadosDoFirebase(id)
         }
-
-
-
-
-
     }
 
-    private fun fetchDadosDoFirebase(docId: String){
+    private fun fetchDadosDoFirebase(docId: String) {
         val docRef = firestore.collection("doenca").document(docId)
         docRef.get()
             .addOnSuccessListener { document ->
-                if (document != null && document.exists()){
+                if (document != null && document.exists()) {
                     val nome = document.getString("nome")
                     val descricao = document.getString("descricao")
                     val url = document.getString("Url")
 
-                    //Carrega a Url de imagem usando o Picasso
-                    url.let {
+                    // Carrega a Url de imagem usando o Picasso
+                    url?.let {
                         Picasso.get().load(it).into(urlImageView)
                     }
 
-                    //Cria um objeto Tipo_Classe com os dados obtidos no Firestore
+                    // Cria um objeto Tipo_Classe com os dados obtidos no Firestore
                     doenca = Tipo_Classe(url ?: "", nome ?: "", descricao ?: "")
 
-                    //Atualiza as TextViews com os dados de nome e descrição
+                    // Atualiza as TextViews com os dados de nome e descrição
                     nomeTextView.text = nome
                     descricaoTextView.text = descricao
-                }else {
+                } else {
                     Log.d("PopupDoenca", "Documento não encontrado")
                 }
             }
             .addOnFailureListener { e ->
-                Log.w("PopupRemedio", "Erro ao obter documento", e)
+                Log.w("PopupDoenca", "Erro ao obter documento", e)
             }
     }
 
+    //Método para o Alert Dialog da doença
+    private fun showConfirmDeleteDialog(doencaId: String){
+        AlertDialog.Builder(this).apply {
+            setTitle("Confirmação de Exclusão")
+            setMessage("Tem certeza que deseja excluir esta doença? Você pode adiciona-la novamente na lista de doenças")
+            setPositiveButton("Sim") { _, _ ->
+                deleteDoencaArray(doencaId)
+            }
+            setNegativeButton("Não", null)
+            create()
+            show()
+        }
+    }
 
+    //Método para excluir a doença individualmente
+    private fun deleteDoencaArray(doencaId: String) {
+        val user = auth.currentUser
+        user?.let {
+            val userDocRef = firestore.collection("clientes").document(it.uid)
+            userDocRef.update("doenca", FieldValue.arrayRemove(doencaId))
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Doença excluida do seu perfil com sucesso", Toast.LENGTH_SHORT).show()
+                    Log.d("DescriçãoDoença_Inicio1", "Sucesso ao remover doença do Array")
+                    onBackPressed()
+                }
+                .addOnFailureListener { e ->
+                    Log.w("DescriçãoDoença_Inicio1", "Erro ao excluir doença do array", e)
+                }
+        }
+    }
 }
-
-
-
-
--- app/src/main/java/com/companyvihva/vihva/model/DescriçãoRemedio_inicio1.kt
+++ app/src/main/java/com/companyvihva/vihva/model/DescriçãoRemedio_inicio1.kt
@@ -2,50 +2,42 @@ package com.companyvihva.vihva.com.companyvihva.vihva.model
 
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.companyvihva.vihva.R
-import com.companyvihva.vihva.model.Tipo_Classe
-import com.companyvihva.vihva.model.Tipo_Remedios
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FieldValue
 import com.google.firebase.firestore.FirebaseFirestore
 import com.squareup.picasso.Picasso
 
-
-
-
 class DescriçãoRemedio_inicio1 : AppCompatActivity() {
 
     private lateinit var firestore: FirebaseFirestore
+    private lateinit var auth: FirebaseAuth
     private lateinit var nomeTextView: TextView
     private lateinit var descricaoTextView: TextView
     private lateinit var urlImageView: ImageView
     private var remedioId: String? = null
-    private var remedioNome: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.popup_desc_remedio_inicio1)
 
-        // Inicializa o Firestore
+        // Inicializa o Firestore e o FirebaseAuth
         firestore = FirebaseFirestore.getInstance()
+        auth = FirebaseAuth.getInstance()
 
-        // Obtém o ID e o nome do remédio passados pelo Intent
+        // Obtém o ID do remédio passado pelo Intent
         remedioId = intent.getStringExtra("remedioId")
-
 
         // Inicializa as views
         nomeTextView = findViewById(R.id.nomere)
         descricaoTextView = findViewById(R.id.descricao1)
         urlImageView = findViewById(R.id.foto_Remedio)
-
-        // Exibe o nome do remédio
-
 
         // Busca os dados do remédio no Firebase usando o ID
         remedioId?.let { id ->
@@ -56,6 +48,14 @@ class DescriçãoRemedio_inicio1 : AppCompatActivity() {
         val btnVoltar = findViewById<ImageButton>(R.id.btn_voltarpop)
         btnVoltar.setOnClickListener {
             onBackPressed()
+        }
+
+        // Configura o botão de excluir
+        val btnExcluir = findViewById<ImageButton>(R.id.lixeira_remedios)
+        btnExcluir.setOnClickListener {
+            remedioId?.let { id ->
+                showConfirmDeleteDialog(id)
+            }
         }
     }
 
@@ -72,9 +72,8 @@ class DescriçãoRemedio_inicio1 : AppCompatActivity() {
                     url?.let {
                         Picasso.get().load(it).into(urlImageView)
                     }
-                    //nome
+                    // Atualiza os TextViews com os dados do remédio
                     nomeTextView.text = nome
-                    // Atualiza o TextView com a descrição do remédio
                     descricaoTextView.text = descricao
                 } else {
                     Log.d("DescriçãoRemedio_inicio1", "Documento não encontrado")
@@ -83,5 +82,37 @@ class DescriçãoRemedio_inicio1 : AppCompatActivity() {
             .addOnFailureListener { e ->
                 Log.w("DescriçãoRemedio_inicio1", "Erro ao obter documento", e)
             }
+    }
+
+    //Método para o Alert Dialog do Remédio
+    private fun showConfirmDeleteDialog(remedioId: String){
+        AlertDialog.Builder(this).apply {
+            setTitle("Confirmação de exclusão")
+            setMessage("Tem certeza que deseja excluir este remédio? Você pode adiciona-lo novamente na lista de remédios")
+            setPositiveButton("Sim") {_, _ ->
+                deleteRemedioArray(remedioId)
+            }
+            setNegativeButton("Não", null)
+            create()
+            show()
+        }
+    }
+
+    // Método para excluir o remédio do array do cliente no Firebase
+    private fun deleteRemedioArray(remedioId: String) {
+        val user = auth.currentUser
+        user?.let {
+            val userDocRef = firestore.collection("clientes").document(it.uid)
+            userDocRef.update("remedios", FieldValue.arrayRemove(remedioId))
+                .addOnSuccessListener {
+                    Toast.makeText(this,"Rememédio excluido do seu perfil com sucesso", Toast.LENGTH_SHORT).show()
+                    Log.d("DescriçãoRemedio_inicio1", "Remédio removido do array com sucesso")
+                    // Voltar para a atividade anterior após a exclusão
+                    onBackPressed()
+                }
+                .addOnFailureListener { e ->
+                    Log.w("DescriçãoRemedio_inicio1", "Erro ao remover remédio do array", e)
+                }
+        }
     }
 }
