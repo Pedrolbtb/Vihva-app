eacf94fb12a05bb4d4fbb93bc11eaaeccdfda227
mumuzinhodo0800 <samuelbueno467@gmail.com>
2024-10-03 10:49:08 -0300

    o cara programa e edita video esse é o foda

--- app/src/main/java/com/companyvihva/vihva/Configurações/Configuracoes_manual_usuario.kt
+++ app/src/main/java/com/companyvihva/vihva/Configurações/Configuracoes_manual_usuario.kt
@@ -1,17 +1,44 @@
 package com.companyvihva.vihva.Configurações
 
+import android.annotation.SuppressLint
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TableRow
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.VideoView
 import com.companyvihva.vihva.R
 
 class Configuracoes_manual_usuario : AppCompatActivity() {
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_configuracoes_manual_usuario)
-    }
-    val manuInicio = findViewById<TableRow>(R.id.ManuInicio).setOnClickListener {
 
-    }
+        // Referências para os componentes do layout
+        val videoView = findViewById<VideoView>(R.id.videoViewDoenca)
+        val playButton = findViewById<Button>(R.id.playButtonDoenca)
 
+        // Definir o caminho do vídeo (substitua pela URI do seu vídeo)
+        val videoPath = "android.resource://" + packageName + "/" + R.raw.doencapreta
+        val uri = Uri.parse(videoPath)
+        videoView.setVideoURI(uri)
+
+        // Reproduzir o vídeo ao clicar no botão
+        playButton.setOnClickListener {
+            videoView.start()
+        }
+
+        // Parar o vídeo se ele terminar
+        videoView.setOnCompletionListener {
+            videoView.seekTo(0)
+        }
+
+        // Listener para fechar a activity ao clicar no botão de voltar
+        val btnClose = findViewById<ImageButton>(R.id.btnClose)
+        btnClose.setOnClickListener {
+            finish()
+        }
+    }
 }
